318f25fdb98d0da2e55830a5cb8f63c4df2e3a4e
Ahmed Yehia <amedyhia23@gmail.com>
2025-03-16 16:44:38 +0200

    adding splash animation

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -2,8 +2,46 @@ import 'package:bookly/core/utilites/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  late Animation<Offset> slidingLogoAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation = Tween<Offset>(
+      begin: Offset(0, 3),
+      end: Offset.zero,
+    ).animate(animationController);
+
+    slidingLogoAnimation = Tween<Offset>(
+      begin: Offset(1, 0),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
+    slidingAnimation.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,13 +50,19 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Spacer(flex: 3),
-        SvgPicture.asset(AssetsData.logo, height: 70, width: 70),
+        SlideTransition(
+          position: slidingLogoAnimation,
+          child: SvgPicture.asset(AssetsData.logo, height: 70, width: 70),
+        ),
         Spacer(flex: 2),
 
-        const Text(
-          'Read Free Books',
-          style: TextStyle(fontSize: 24),
-          textAlign: TextAlign.center,
+        SlideTransition(
+          position: slidingAnimation,
+          child: const Text(
+            'Read Free Books',
+            style: TextStyle(fontSize: 24),
+            textAlign: TextAlign.center,
+          ),
         ),
         Spacer(flex: 1),
       ],
