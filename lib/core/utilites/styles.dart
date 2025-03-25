808bd4d1999868baf2d1df86ef72aab7f8909f40
Ahmed Yehia <amedyhia23@gmail.com>
2025-03-25 15:44:56 +0200

    add title and subtitle to detaile

--- lib/core/utilites/styles.dart
+++ lib/core/utilites/styles.dart
@@ -5,12 +5,20 @@ abstract class Styles {
     fontSize: 20,
     fontWeight: FontWeight.w600,
   );
+  static const textStyle30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w600,
+  );
   static const textStyle14 = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.normal,
   );
   static const textStyle16 = TextStyle(
     fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+  static const textStyle18 = TextStyle(
+    fontSize: 18,
     fontWeight: FontWeight.bold,
   );
 }
--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:bookly/constans.dart';
+import 'package:bookly/core/utilites/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_details_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
@@ -15,10 +17,29 @@ class BookDetailsViewBody extends StatelessWidget {
           BookDetailsAppBar(),
           Padding(
             padding: EdgeInsets.symmetric(
-              horizontal: screenwidth * 0.17,
+              horizontal: screenwidth * 0.2,
               vertical: 30,
             ),
             child: CustomBookImage(),
+          ),
+          const SizedBox(height: 13),
+          Text(
+            'The Jungle Book',
+            style: Styles.textStyle30.copyWith(
+              fontFamily: kGTSectraFine,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 6),
+          Opacity(
+            opacity: 0.7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic,
+                fontFamily: kGTSectraFine,
+              ),
+            ),
           ),
         ],
       ),
