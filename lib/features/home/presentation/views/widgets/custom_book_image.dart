0814b0fd5af66148c9bf1ff6deb43a51994a0d6f
Ahmed Yehia <amedyhia23@gmail.com>
2025-04-04 23:15:56 +0200

    cashed network image

--- lib/features/home/presentation/views/widgets/custom_book_image.dart
+++ lib/features/home/presentation/views/widgets/custom_book_image.dart
@@ -1,5 +1,5 @@
-import 'package:bookly/core/utilites/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class CustomBookImage extends StatelessWidget {
   const CustomBookImage({super.key, required this.urlImage});
@@ -8,14 +8,20 @@ class CustomBookImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 2.6 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          image: DecorationImage(
-            image: NetworkImage(urlImage),
-            fit: BoxFit.fill,
-          ),
-        ),
+      child: CachedNetworkImage(
+        imageUrl: urlImage,
+        placeholder:
+            (context, url) => const Center(child: CircularProgressIndicator()),
+        errorWidget:
+            (context, url, error) => const Icon(Icons.error_outline, size: 40),
+        fit: BoxFit.fill,
+        imageBuilder:
+            (context, imageProvider) => Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                image: DecorationImage(image: imageProvider, fit: BoxFit.fill),
+              ),
+            ),
       ),
     );
   }
